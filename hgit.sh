f43458ffbaa64989e2f97642388d2db134e08f18
Michael Ziegler <michael.ziegler@proemion.com>
2021-08-04 11:39:53 +0200

    add safeguard against commiting files while others are staged

--- hgit.sh
+++ hgit.sh
@@ -377,6 +377,8 @@ function hgit_commit {
     done
     if hgit_have_fork && [ "$(hgit_branch)" = "$MASTER_BRANCH" ]; then
         echo "You have a fork and you're commiting to $MASTER_BRANCH. You probably don't want to do that, aborting."
+    elif git status --short | grep -q '^M' && [ "${#FILES[@]}" -gt 0 ]; then
+        echo "There are changes in the staging area (see h dc), but you also gave a list of files on the command line. This will probably commit more changes than you intend to, aborting."
     elif [ -n "$MESSAGE" ]; then
         git commit -m "$MESSAGE" -- "${FILES[@]}"
     else
