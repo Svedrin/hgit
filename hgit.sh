c8f49d543cca39955b001088bca0814931bfb51e
Michael Ziegler <ich@michaelziegler.name>
2026-09-18 20:23:16 +0200

    safeguard against AGENT_ARGS being empty

--- hgit.sh
+++ hgit.sh
@@ -948,9 +948,9 @@ function hgit_agent {
         read -r AGENT_CMD
     fi
     if command -v sbx &>/dev/null; then
-        exec sbx run "$AGENT_CMD" . "$MAIN_WT:ro" "$MAIN_WT/.git" -- $AGENT_ARGS
+        exec sbx run "$AGENT_CMD" . "$MAIN_WT:ro" "$MAIN_WT/.git" -- ${AGENT_ARGS:-}
     else
-        exec "$AGENT_CMD" $AGENT_ARGS
+        exec "$AGENT_CMD" ${AGENT_ARGS:-}
     fi
 }
 
